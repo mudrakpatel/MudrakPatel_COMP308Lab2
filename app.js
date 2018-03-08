b0b6a846a30c4877ad478d243f92df0db2a5dc3c
mudrakpatel <mudrakpatel96@gmail.com>
2018-03-08 00:48:39 -0500

    Node server runs fine

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ var app = express(); //express constructor function
 app.set('view engine', 'ejs');
 //set Templates directory as views folder for ejs view engine
 app.set('views', path.join(__dirname, '/Templates'));
+//Handle the Home page route (Temporarily ONLY)
+app.get("/", function(request, response){
+    response.send("<h1>Welcome!</h1>");
+});
 app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"));
