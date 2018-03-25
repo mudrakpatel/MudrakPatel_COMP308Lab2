84e87b69e9282e5623c12f6cc1a64783b976a474
mudrakpatel <mudrakpatel96@gmail.com>
2018-03-25 13:25:21 -0400

    Login works and now working on adding user feedback to database

--- Routes/login.js
+++ Routes/login.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var router = express.Router();
+var CustomerModel = require("../Models/Customer.model.js");
 
-router.get("/login", function(request, response){
+router.get("/login", function(request, response) {
     response.render("login.ejs");
 });
 
-router.post("/login", function(request, response){
-    
+router.post("/login", function(request, response) {
+    var tempEmail = request.body.userEmailID;
+    var tempPassword = request.body.userPassword;
+    CustomerModel.findOne({ EmailID: tempEmail, Password: tempPassword },
+        function(error, customer) {
+            if (error) {
+                console.log(">>> ERROR: " + error.message);
+                response.status(200).send(error);
+            } else {
+                console.log(customer);
+                response.render("feedback.ejs", { routeCustomer: customer });
+            }
+        });
 });
 
 module.exports = router;
